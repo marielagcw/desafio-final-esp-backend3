b84f144da7e857af22d0c0dfb37f99585e4b9db9
Marielagcw <marielagcw@hotmail.com>
2023-09-15 14:20:14 -0300

    Correcciones para el funcionamiento de la app

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,40 @@
-package server
+package main
+
+import (
+	"log"
+	"os"
+
+	"desafio-final/cmd/server/routes"
+	"github.com/gin-gonic/gin"
+)
+
+const port = ":8080"
+
+func main() {
+
+	// Recover from panic
+	defer func() {
+		if err := recover(); err != nil {
+			log.Fatal(err)
+			os.Exit(1)
+		}
+	}()
+
+	// Router
+	engine := gin.New()
+	engine.Use(gin.Recovery())
+
+	// Server
+	runApp(engine)
+}
+
+func runApp(engine *gin.Engine) {
+	// Corremos el servidor
+	router := routes.NewRouter(engine)
+	// Mapeamos las rutas
+	router.MapRoutes()
+	if err := engine.Run(port); err != nil {
+		panic(err)
+	}
+
+}
