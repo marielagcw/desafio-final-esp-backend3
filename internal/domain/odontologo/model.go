ab4f55dfbd6497a79cdd89bd95f9ee580a0fcd41
Marielagcw <marielagcw@hotmail.com>
2023-09-16 13:01:43 -0300

    Se agregan estructuras para odontologo

--- internal/domain/odontologo/model.go
+++ internal/domain/odontologo/model.go
@@ -1,1 +1,17 @@
 package domain
+
+// Odontologo is a struct that represents a Odontologo
+type Odontologo struct {
+	ID			int64  `json:"id"`
+	Apellido 	string `json:"apellido"`
+	Nombre 		string `json:"nombre"`
+	Matricula 	string `json:"matricula"`
+}
+
+// RequestOdontologo is a struct that represents a Request
+type RequestOdontologo struct {
+	Apellido 	string `json:"apellido"`
+	Nombre 		string `json:"nombre"`
+	Matricula 	string `json:"matricula"`
+}
+
