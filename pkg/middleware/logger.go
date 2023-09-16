43cb2c443fc5d64dc6f05b05a58d918c0ae2cd57
Marielagcw <marielagcw@hotmail.com>
2023-09-15 21:06:48 -0300

    Se agrega logger

--- pkg/middleware/logger.go
+++ pkg/middleware/logger.go
@@ -1,1 +1,30 @@
 package middleware
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	basePath = "http://localhost:8080"
+)
+
+func Logger() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		path := ctx.Request.URL
+		time := time.Now()
+		method := ctx.Request.Method
+
+		ctx.Next()
+
+		var size int
+
+		if ctx.Writer != nil {
+			size = ctx.Writer.Size()
+		}
+
+		fmt.Printf("Path: %s%s\nMethod: %s\nTime: %s\nSize: %d\n", basePath, path, method, time, size)
+	}
+}
